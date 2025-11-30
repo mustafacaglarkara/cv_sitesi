6d8bce34222bba8cd1577ec0aeebfe5415c46789
mustafacaglarkara <mustafacaglarkara@gmail.com>
2025-11-30 11:07:27 +0300

    .env dosyası ayarlandı

---
+++ home/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def index(request):
+    context = {
+        'title': "Anasayfa",
+        'isim': "Mustafa Çağlar KARA"
+    }
+    return render(request, 'index.html', context)
